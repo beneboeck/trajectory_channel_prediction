34124237d5602b274965e30e6fce196cb6d9e54b
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-07-26 18:11:28 +0200

    Update main_unified.py

--- main_unified.py
+++ main_unified.py
@@ -24,7 +24,7 @@ m_file = open(dir_path + '/m_file.txt','w')
 device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
 
 BATCHSIZE = 50
-G_EPOCHS = 1
+G_EPOCHS = 3
 LEARNING_RATE = 3e-5
 FREE_BITS_LAMBDA = torch.tensor(1).to(device) # is negligible if free bits isn't used
 SNAPSHOTS = 20 # 96 / 192 should be taken for all models expect the modelbased one
