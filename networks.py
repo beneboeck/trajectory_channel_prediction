35344f5961af09124223d578b5a8b4d22acf0911
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-07-27 10:00:44 +0200

    new

--- networks.py
+++ networks.py
@@ -1900,11 +1900,13 @@ class Prior(nn.Module):
         self.net = []
         self.net.append(nn.Linear(ld,pr_width * ld))
         self.net.append(nn.ReLU())
-        self.net.append(nn.BatchNorm1d(pr_width * ld,track_running_stats=False))
+        #self.net.append(nn.BatchNorm1d(pr_width * ld,track_running_stats=False))
+        self.net.append(nn.BatchNorm1d(pr_width * ld, eps=1e-4))
         for l in range(pr_layer-2):
             self.net.append(nn.Linear(pr_width * ld, pr_width * ld))
             self.net.append(nn.ReLU())
-            self.net.append(nn.BatchNorm1d(pr_width * ld,track_running_stats=False))
+            #self.net.append(nn.BatchNorm1d(pr_width * ld,track_running_stats=False))
+            self.net.append(nn.BatchNorm1d(pr_width * ld, eps=1e-4))
         self.net.append(nn.Linear(pr_width * ld,2 * ld))
         self.hidden_to_out = nn.Linear(self.ld, 2 * self.ld)
         self.net = nn.Sequential(*self.net)
@@ -1943,17 +1945,20 @@ class Encoder(nn.Module):
         self.x_prenet = nn.Sequential(
             nn.Linear(n_ant * 2 * (memory+1),int(n_ant * 2 * (memory+1) - step)),
             nn.ReLU(),
-            nn.BatchNorm1d(int(n_ant * 2 * (memory+1) - step),track_running_stats=False),
+            #nn.BatchNorm1d(int(n_ant * 2 * (memory+1) - step),track_running_stats=False),
+            nn.BatchNorm1d(int(n_ant * 2 * (memory + 1) - step), eps=1e-4),
             nn.Linear(int(n_ant * 2 * (memory+1) - step),2*ld),)
 
         self.net = []
         self.net.append(nn.Linear(3 * ld,en_width * ld))
         self.net.append(nn.ReLU())
-        self.net.append(nn.BatchNorm1d(en_width * ld,track_running_stats=False))
+        #self.net.append(nn.BatchNorm1d(en_width * ld,track_running_stats=False))
+        self.net.append(nn.BatchNorm1d(en_width * ld, eps=1e-4))
         for l in range(en_layer-2):
             self.net.append(nn.Linear(en_width * ld, en_width * ld))
             self.net.append(nn.ReLU())
-            self.net.append(nn.BatchNorm1d(en_width * ld,track_running_stats=False))
+            #self.net.append(nn.BatchNorm1d(en_width * ld,track_running_stats=False))
+            self.net.append(nn.BatchNorm1d(en_width * ld, eps=1e-4))
         self.net.append(nn.Linear(en_width * ld,2 * ld))
         self.hidden_to_out = nn.Linear(self.ld,2*self.ld)
 
@@ -2000,11 +2005,13 @@ class Decoder(nn.Module):
         net_out_dim = int(de_width * ld * (memory+1) - step)
         self.net.append(nn.Linear(ld*(memory+1),net_in_dim))
         self.net.append(nn.ReLU())
-        self.net.append(nn.BatchNorm1d(net_in_dim,track_running_stats=False))
+        #self.net.append(nn.BatchNorm1d(net_in_dim,track_running_stats=False))
+        self.net.append(nn.BatchNorm1d(net_in_dim, eps=1e-4))
         for l in range(de_layer-2):
             self.net.append(nn.Linear(net_in_dim,net_out_dim))
             self.net.append(nn.ReLU())
-            self.net.append(nn.BatchNorm1d(net_out_dim,track_running_stats=False))
+            #self.net.append(nn.BatchNorm1d(net_out_dim,track_running_stats=False))
+            self.net.append(nn.BatchNorm1d(net_out_dim, eps=1e-4))
             net_in_dim = net_out_dim
             net_out_dim = int(net_out_dim - step)
         self.net.append(nn.Linear(net_in_dim,output_dim))
