2d9ce5bd8639682064a1eb41eff9130a017c3f99
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-07-27 15:00:53 +0200

    Update evaluation_unified.py

--- evaluation_unified.py
+++ evaluation_unified.py
@@ -212,16 +212,16 @@ def channel_estimation(setup,model,dataloader_val,sig_n,dir_path,device):
             Gamma[torch.abs(torch.imag(Gamma)) < 10 ** (-5)] = torch.real(Gamma[torch.abs(torch.imag(Gamma)) < 10 ** (-5)]) + 0j
 
             L,U = torch.linalg.eigh(Gamma)
-            Cov_out = U @ torch.diag_embed(1/L) @ U.mH
-            L_noisy = L + (sig_n**2 * torch.eye(32,32).to(device))[None,None,:,:]
+            Cov_out = U @ torch.diag_embed(1/L).cfloat() @ U.mH
+            L_noisy = (L + (sig_n**2 * torch.eye(32,32).to(device))[None,None,:,:]).cfloat()
 
             h_hat = mu_compl + torch.einsum('ijkl,ijl->ijk',Cov_out @ (U @ torch.diag_embed(L_noisy) @ U.mH), (noisy_sample_compl - mu_compl))
             h_hat_last = h_hat[:,-1,:]
 
 
         if (cov_type == 'diagonal') | (cov_type == 'DFT'):
-            Cov_out = torch.diag_embed(1/(torch.exp(logpre_out.permute(0,2,1))))
-            inv_matrix = 1/Cov_out + (sig_n**2 * torch.eye(32,32).to(device))[None,None,:,:]
+            Cov_out = torch.diag_embed(1/(torch.exp(logpre_out.permute(0,2,1)))).cfloat()
+            inv_matrix = 1/Cov_out + (sig_n**2 * torch.eye(32,32).to(device)).cfloat()[None,None,:,:]
 
             print('hier')
             print(mu_compl.size())
