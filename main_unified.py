5c1a054079b7d54dc2ef6cf76df21bf1b29a16a7
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-08-16 09:42:46 +0200

    Update main_unified.py

--- main_unified.py
+++ main_unified.py
@@ -13,7 +13,7 @@ import evaluation_unified as ev
 import math
 
 # GLOBAL PARAMETERS
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 BATCHSIZE = 50
 G_EPOCHS = 700
 LEARNING_RATE = 6e-5
