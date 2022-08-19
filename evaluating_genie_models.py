df2728de2cffcb63daa27bbbe12a443566891fca
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-08-19 13:32:09 +0200

    Update evaluating_genie_models.py

--- evaluating_genie_models.py
+++ evaluating_genie_models.py
@@ -27,7 +27,7 @@ n_permutations = 300
 bs_mmd = 1000
 normed = False
 author = 'Bene'
-SNR_db = 5
+SNR_db_list = [-10,-5,0,5,10,15,20]
 
 
 path_DFT_Tra = '/home/ga42kab/lrz-nashome/trajectory_channel_prediction/models/time_17_00_tra/model_dict'
@@ -55,7 +55,7 @@ glob_file.write(f'\AUTHER: {author}\n\n')
 glob_file.write('BATCHSIZE: ' + str(BATCHSIZE) +'\n')
 glob_file.write('G_EPOCHS: ' +str(G_EPOCHS) +'\n')
 glob_file.write(f'Learning Rate: {LEARNING_RATE}\n')
-glob_file.write(f'SNR_db: {SNR_db}\n')
+glob_file.write(f'SNR_db_list: {SNR_db_list}\n')
 glob_file.write(f'n_iterations: {n_iterations}\n')
 glob_file.write(f'n_permutations: {n_permutations}\n\n')
 log_file.write('Date: ' +date +'\n')
@@ -89,49 +89,54 @@ model_TD_VAE = mg.my_VAE(cov_type,LD_VAE,conv_layer,total_layer,out_channel,k_si
 model_TD_VAE.load_state_dict(torch.load(path_TD_VAE,map_location=device))
 
 
-if DATASET_TYPE == 'my_Quadriga':
-    H_test = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_test.npy','r')
-    H_train = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_train.npy','r')
-    H_val = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_val.npy','r')
-    pg_test = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_test.npy','r')
-    pg_train = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_train.npy','r')
-    pg_val = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_val.npy','r')
 
-    H_test = H_test/np.sqrt(10**(0.1 * pg_test[:,None,None,0:1]))
-    H_val = H_val/np.sqrt(10**(0.1 * pg_val[:,None,None,0:1]))
-    H_train = H_train/np.sqrt(10**(0.1 * pg_train[:,None,None,0:1]))
+for SNR_db in SNR_db_list:
 
-    print(np.mean(np.sum(np.abs(H_train)**2,axis=(1,2))))
-    print(np.mean(H_train))
-    print(np.std(H_train))
+    if DATASET_TYPE == 'my_Quadriga':
+        H_test = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_test.npy','r')
+        H_train = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_train.npy','r')
+        H_val = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/H_val.npy','r')
+        pg_test = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_test.npy','r')
+        pg_train = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_train.npy','r')
+        pg_val = np.load('/home/ga42kab/lrz-nashome/trajectory_channel_prediction/data/my_quadriga/pg_val.npy','r')
 
-    H_test_dft = apply_DFT(H_test)
-    H_val_dft = apply_DFT(H_val)
-    H_train_dft = apply_DFT(H_train)
+        H_test = H_test/np.sqrt(10**(0.1 * pg_test[:,None,None,0:1]))
+        H_val = H_val/np.sqrt(10**(0.1 * pg_val[:,None,None,0:1]))
+        H_train = H_train/np.sqrt(10**(0.1 * pg_train[:,None,None,0:1]))
+
+        print(np.mean(np.sum(np.abs(H_train)**2,axis=(1,2))))
+        print(np.mean(H_train))
+        print(np.std(H_train))
+
+        H_test_dft = apply_DFT(H_test)
+        H_val_dft = apply_DFT(H_val)
+        H_train_dft = apply_DFT(H_train)
+
+        x_train = np.mean(np.sum(H_train[:,:,:,-1]**2,axis=(1,2)))
+        SNR_eff = 10**(SNR_db/10)
+        sig_n_train = math.sqrt(x_train/(32 * SNR_eff))
+        x_test = np.mean(np.sum(H_test[:,:,:,-1]**2,axis=(1,2)))
+        SNR_eff = 10**(SNR_db/10)
+        sig_n_test = math.sqrt(x_test/(32 * SNR_eff))
+        x_val = np.mean(np.sum(H_val[:,:,:,-1]**2,axis=(1,2)))
+        SNR_eff = 10**(SNR_db/10)
+        sig_n_val = math.sqrt(x_val/(32 * SNR_eff))
+
+        n_H_train = H_train + sig_n_train/math.sqrt(2) * np.random.randn(*H_train.shape)
+        n_H_test = H_test + sig_n_test/math.sqrt(2) * np.random.randn(*H_test.shape)
+        n_H_val = H_val + sig_n_val/math.sqrt(2) * np.random.randn(*H_val.shape)
+        n_H_train_dft = H_train_dft + sig_n_train/math.sqrt(2) * np.random.randn(*H_train_dft.shape)
+        n_H_test_dft = H_test_dft + sig_n_test/math.sqrt(2) * np.random.randn(*H_test_dft.shape)
+        n_H_val_dft = H_val_dft + sig_n_val/math.sqrt(2) * np.random.randn(*H_val_dft.shape)
+
+    dataset_test = ds.dataset(H_test,H_test_dft,n_H_test, n_H_test_dft)
+    dataset_train = ds.dataset(H_train,H_train_dft,n_H_train, n_H_train_dft)
+    dataset_val = ds.dataset(H_val,H_val_dft,n_H_val, n_H_val_dft)
+
+    dataloader_test = DataLoader(dataset_test,shuffle=True,batch_size= len(dataset_test))
+    dataloader_train = DataLoader(dataset_train,shuffle=True,batch_size=BATCHSIZE)
+    dataloader_val = DataLoader(dataset_val,shuffle=True,batch_size= len(dataset_val))
 
 
-    x_train = np.mean(np.sum(H_train[:,:,:,-1]**2,axis=(1,2)))
-    SNR_eff = 10**(SNR_db/10)
-    sig_n_train = math.sqrt(x_train/(32 * SNR_eff))
-    x_test = np.mean(np.sum(H_test[:,:,:,-1]**2,axis=(1,2)))
-    SNR_eff = 10**(SNR_db/10)
-    sig_n_test = math.sqrt(x_test/(32 * SNR_eff))
-    x_val = np.mean(np.sum(H_val[:,:,:,-1]**2,axis=(1,2)))
-    SNR_eff = 10**(SNR_db/10)
-    sig_n_val = math.sqrt(x_val/(32 * SNR_eff))
-
-    n_H_train = H_train + sig_n_train/math.sqrt(2) * np.random.randn(*H_train.shape)
-    n_H_test = H_test + sig_n_test/math.sqrt(2) * np.random.randn(*H_test.shape)
-    n_H_val = H_val + sig_n_val/math.sqrt(2) * np.random.randn(*H_val.shape)
-    n_H_train_dft = H_train_dft + sig_n_train/math.sqrt(2) * np.random.randn(*H_train_dft.shape)
-    n_H_test_dft = H_test_dft + sig_n_test/math.sqrt(2) * np.random.randn(*H_test_dft.shape)
-    n_H_val_dft = H_val_dft + sig_n_val/math.sqrt(2) * np.random.randn(*H_val_dft.shape)
-
-dataset_test = ds.dataset(H_test,H_test_dft,n_H_test, n_H_test_dft)
-dataset_train = ds.dataset(H_train,H_train_dft,n_H_train, n_H_train_dft)
-dataset_val = ds.dataset(H_val,H_val_dft,n_H_val, n_H_val_dft)
-
-dataloader_test = DataLoader(dataset_test,shuffle=True,batch_size= len(dataset_test))
-dataloader_train = DataLoader(dataset_train,shuffle=True,batch_size=BATCHSIZE)
-dataloader_val = DataLoader(dataset_val,shuffle=True,batch_size= len(dataset_val))
-
+    NMSE_test_est = ev.channel_estimation(model_TD_Tra, dataloader_test, sig_n_test, 'Toeplitz', dir_path, device)
+    print(NMSE_test_est)
