b747b5e5893786b6a7088cf0b3c08d6caef631f1
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-09-07 11:44:11 +0200

    Update main_unified.py

--- main_unified.py
+++ main_unified.py
@@ -90,7 +90,7 @@ if MODEL_TYPE == 'Trajectory':
     glob_file.write(f'UB_var_dec: {UB_var_dec:.4f}\n')
 if MODEL_TYPE == 'Single':
     LD_VAE, conv_layer, total_layer, out_channel, k_size, cov_type,prepro,LB_var_dec,UB_var_dec,BN = network_architecture_search_VAE()
-    LD_VAE, conv_layer, total_layer, out_channel, k_size, cov_type, prepro, LB_var_dec, UB_var_dec, BN = 56,0,5,128,9,'Toeplitz','None',0.007,0.6912,False
+    LD_VAE, conv_layer, total_layer, out_channel, k_size, cov_type, prepro, LB_var_dec, UB_var_dec, BN = 16,1,3,128,7,'Toeplitz','None',0.0061,0.5405,True
     setup = [LD_VAE, conv_layer, total_layer, out_channel, k_size, cov_type,prepro]
     print('Single Setup')
     print(LD_VAE,conv_layer,total_layer,out_channel,k_size,cov_type,prepro,LB_var_dec,UB_var_dec,BN)
