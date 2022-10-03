d02b47d74b357e4de4b016d628acf251ba9bf146
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-10-03 20:35:44 +0200

    Update main_unified.py

--- main_unified.py
+++ main_unified.py
@@ -20,8 +20,8 @@ BATCHSIZE = 50
 G_EPOCHS = 600
 LEARNING_RATE = 6e-5
 FREE_BITS_LAMBDA = torch.tensor(0.1).to(device)
-SNAPSHOTS = 16
-MODEL_TYPE = 'TraSingle' #Trajectory,Single,TraSingle
+SNAPSHOTS = 8
+MODEL_TYPE = 'Trajectory' #Trajectory,Single,TraSingle
 n_iterations = 75
 n_permutations = 300
 bs_mmd = 1000
@@ -45,8 +45,8 @@ overall_path = '/home/ga42kab/lrz-nashome/trajectory_channel_prediction/'
 dir_path = '/home/ga42kab/lrz-nashome/trajectory_channel_prediction/models/time_' + time
 os.mkdir (dir_path)
 
-if not(exists(overall_path + MODEL_TYPE + '_' + 'NEW_RANGE_noise_NAS_file.txt')):
-    csvfile = open(overall_path + MODEL_TYPE + '_' + 'NEW_RANGE_noise_NAS_file.txt','w')
+if not(exists(overall_path + MODEL_TYPE + '_' + '8_SNAPS_RANGE_noise_NAS_file.txt')):
+    csvfile = open(overall_path + MODEL_TYPE + '_' + '8_SNAPS_RANGE_noise_NAS_file.txt','w')
     csv_writer = csv.writer(csvfile)
     if MODEL_TYPE == 'Trajectory':
         csv_writer.writerow(['Time','LD', 'memory', 'rnn_bool', 'en_layer', 'en_width', 'pr_layer', 'pr_width', 'de_layer', 'de_width', 'cov_type', 'BN', 'prepro','DecVarLB','DecVarUB','TPR','TPRinf','Risk_val','NMSE_0dB','NMSE_5dB','NMSE_10dB','NMSE_20dB'])
@@ -293,7 +293,7 @@ for SNR_db in SNR_db_list:
 
 
 
-csv_file = open(overall_path + MODEL_TYPE + '_' + 'NEW_RANGE_noise_NAS_file.txt','a')
+csv_file = open(overall_path + MODEL_TYPE + '_' + '8_SNAPS_RANGE_noise_NAS_file.txt','a')
 csv_writer = csv.writer(csv_file)
 if MODEL_TYPE == 'Trajectory':
     csv_writer.writerow([time,LD, memory, rnn_bool, en_layer, en_width, pr_layer, pr_width, de_layer, de_width, cov_type, BN, prepro,LB_var_dec,UB_var_dec,TPR1_val,TPR2_val,round(Risk_val.item(),3),round(NMSE_est[0],5),round(NMSE_est[1],5),round(NMSE_est[2],5),round(NMSE_est[3],5)])
